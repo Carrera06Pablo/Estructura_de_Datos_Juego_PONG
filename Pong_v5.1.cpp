cf0193e47b376c446fece5ea94746f2bd8b0f0ae
Davidmoran9 <davidleonel0408@gmail.com>
2024-09-02 19:16:19 -0500

    Version 5.2.0

--- Pong_v5.1.cpp
+++ Pong_v5.1.cpp
@@ -9,6 +9,7 @@ const int SCREEN_HEIGHT = 480;
 
 // Constantes de la pelota
 const int BALL_SIZE = 15;
+
 int ballX, ballY;
 int ballSpeedX = 6, ballSpeedY = 6;
 
@@ -19,9 +20,14 @@ int paddle1Y, paddle2Y;
 int paddleSpeed = 5;
 
 // Puntuación
+
+
 int score1 = 0, score2 = 0;
+const int WINNING_SCORE = 10;
 
 // Variables de audio
+
+
 SDL_AudioSpec wavSpecPaddle, wavSpecScore, wavSpecImpact;
 Uint32 wavLengthPaddle, wavLengthScore, wavLengthImpact;
 Uint8 *wavBufferPaddle, *wavBufferScore, *wavBufferImpact;
@@ -44,6 +50,7 @@ bool init(SDL_Window** window, SDL_Renderer** renderer, TTF_Font** font) {
     }
 
     *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+
     if (*renderer == NULL) {
         std::cout << "El renderizador no pudo ser creado. SDL_Error: " << SDL_GetError() << std::endl;
         return false;
@@ -85,6 +92,8 @@ bool init(SDL_Window** window, SDL_Renderer** renderer, TTF_Font** font) {
 }
 
 // Liberar los recursos
+
+
 void close(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font) {
     // Liberar sonidos
     SDL_CloseAudioDevice(deviceIdPaddle);
@@ -105,6 +114,7 @@ void close(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font) {
 }
 
 // Renderizar texto en pantalla
+
 void renderText(SDL_Renderer* renderer, TTF_Font* font, std::string text, int x, int y) {
     SDL_Color color = {255, 255, 255};
     SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
@@ -118,11 +128,13 @@ void renderText(SDL_Renderer* renderer, TTF_Font* font, std::string text, int x,
 }
 
 // Actualizar la posición de la pelota
+
 void updateBall() {
     ballX += ballSpeedX;
     ballY += ballSpeedY;
 
     // Colisiones con los bordes superior e inferior
+
     if (ballY <= 0 || ballY >= SCREEN_HEIGHT - BALL_SIZE) {
         ballSpeedY = -ballSpeedY;
         SDL_QueueAudio(deviceIdImpact, wavBufferImpact, wavLengthImpact);
@@ -135,9 +147,12 @@ void updateBall() {
         ballSpeedX = -ballSpeedX;
         SDL_QueueAudio(deviceIdPaddle, wavBufferPaddle, wavLengthPaddle);
         SDL_PauseAudioDevice(deviceIdPaddle, 0);  // Reproducir sonido al golpear la paleta
+
+
     }
 
     // Reiniciar la pelota y actualizar el marcador si sale de los límites
+
     if (ballX < 0) {
         score2++;
         ballX = (SCREEN_WIDTH - BALL_SIZE) / 2;
@@ -155,6 +170,20 @@ void updateBall() {
     }
 }
 
+// Comprobar si alguien ha ganadosadsa
+
+
+bool checkWinCondition(SDL_Renderer* renderer, TTF_Font* font) {
+    if (score1 == WINNING_SCORE || score2 == WINNING_SCORE) {
+        std::string winnerText = (score1 == WINNING_SCORE) ? "Jugador 1 Gana!" : "Jugador 2 Gana!";
+        renderText(renderer, font, winnerText, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(3000); // Esperar 3 segundos antes de volver al menú
+        return true;
+    }
+    return false;
+}
+
 // Actualizar la posición de la paleta controlada por la computadora
 void updateAIPaddle() {
     if (ballY < paddle2Y + PADDLE_HEIGHT / 2) {
@@ -169,7 +198,10 @@ void render(SDL_Renderer* renderer, TTF_Font* font) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
+
+
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
 
     // Renderizar la pelota
     SDL_Rect ballRect = {ballX, ballY, BALL_SIZE, BALL_SIZE};
@@ -182,37 +214,47 @@ void render(SDL_Renderer* renderer, TTF_Font* font) {
     SDL_Rect paddle2Rect = {SCREEN_WIDTH - PADDLE_WIDTH, paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT};
     SDL_RenderFillRect(renderer, &paddle2Rect);
 
-    // Renderizar el marcador
+    // Renderizar la puntuación
     renderText(renderer, font, std::to_string(score1), SCREEN_WIDTH / 4, 20);
     renderText(renderer, font, std::to_string(score2), SCREEN_WIDTH * 3 / 4, 20);
 
     SDL_RenderPresent(renderer);
 }
 
-// Mostrar las instrucciones
+// Mostrar las instrucciones del juego
 void showInstructions(SDL_Renderer* renderer, TTF_Font* font) {
-    bool waiting = true;
     SDL_Event e;
+    bool waiting = true;
 
     while (waiting) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
         renderText(renderer, font, "Instrucciones:", 50, 50);
+
         renderText(renderer, font, "Lógica del Juego:", 50, 100);
+
         renderText(renderer, font, "Contra otro Jugador: Ambos jugadores", 50, 140);
+
         renderText(renderer, font, "controlan las paletas usando W/S para el", 50, 180);
+
         renderText(renderer, font, "jugador 1 y UP/DOWN para el jugador 2.", 50, 220);
+
         renderText(renderer, font, "Contra la Computadora: El jugador 1", 50, 260);
+
         renderText(renderer, font, "controla la paleta izquierda, mientras que", 50, 300);
+
         renderText(renderer, font, "la paleta derecha es controlada", 50, 340);
+
         renderText(renderer, font, "automaticamente.", 50, 380);
         renderText(renderer, font, "Presione cualquier tecla para volver al menu.", 50, 420);
 
         SDL_RenderPresent(renderer);
 
         while (SDL_PollEvent(&e) != 0) {
+
             if (e.type == SDL_QUIT) {
+
                 waiting = false;
                 break;
             }
@@ -268,6 +310,18 @@ bool showMenu(SDL_Renderer* renderer, TTF_Font* font) {
     return true;
 }
 
+// Reiniciar variables
+void resetGame() {
+    paddle1Y = (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2;
+    paddle2Y = (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2;
+    ballX = (SCREEN_WIDTH - BALL_SIZE) / 2;
+    ballY = (SCREEN_HEIGHT - BALL_SIZE) / 2;
+    ballSpeedX = 6;
+    ballSpeedY = 6;
+    score1 = 0;
+    score2 = 0;
+}
+
 int main(int argc, char* args[]) {
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
@@ -278,53 +332,59 @@ int main(int argc, char* args[]) {
         return -1;
     }
 
-    if (!showMenu(renderer, font)) {
-        close(window, renderer, font);
-        return 0;
-    }
-
-    paddle1Y = (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2;
-    paddle2Y = (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2;
-    ballX = (SCREEN_WIDTH - BALL_SIZE) / 2;
-    ballY = (SCREEN_HEIGHT - BALL_SIZE) / 2;
-
     bool quit = false;
-    SDL_Event e;
-
     while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
+        if (!showMenu(renderer, font)) {
+            break;
         }
 
-        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-        if (currentKeyStates[SDL_SCANCODE_W]) {
-            paddle1Y -= paddleSpeed;
-            if (paddle1Y < 0) paddle1Y = 0;
-        }
-        if (currentKeyStates[SDL_SCANCODE_S]) {
-            paddle1Y += paddleSpeed;
-            if (paddle1Y > SCREEN_HEIGHT - PADDLE_HEIGHT) paddle1Y = SCREEN_HEIGHT - PADDLE_HEIGHT;
-        }
-        if (!playAgainstAI) {
-            if (currentKeyStates[SDL_SCANCODE_UP]) {
-                paddle2Y -= paddleSpeed;
-                if (paddle2Y < 0) paddle2Y = 0;
-            }
-            if (currentKeyStates[SDL_SCANCODE_DOWN]) {
-                paddle2Y += paddleSpeed;
-                if (paddle2Y > SCREEN_HEIGHT - PADDLE_HEIGHT) paddle2Y = SCREEN_HEIGHT - PADDLE_HEIGHT;
-            }
-        } else {
-            updateAIPaddle();
-        }
+        resetGame();
+        bool gameRunning = true;
 
-        updateBall();
-        render(renderer, font);
-        SDL_Delay(10);
+        while (gameRunning) {
+            SDL_Event e;
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    gameRunning = false;
+                    quit = true;
+                }
+            }
+
+            const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+            if (currentKeyStates[SDL_SCANCODE_W]) {
+                paddle1Y -= paddleSpeed;
+                if (paddle1Y < 0) paddle1Y = 0;
+            }
+            if (currentKeyStates[SDL_SCANCODE_S]) {
+                paddle1Y += paddleSpeed;
+                if (paddle1Y > SCREEN_HEIGHT - PADDLE_HEIGHT) paddle1Y = SCREEN_HEIGHT - PADDLE_HEIGHT;
+            }
+            if (!playAgainstAI) {
+                if (currentKeyStates[SDL_SCANCODE_UP]) {
+                    paddle2Y -= paddleSpeed;
+                    if (paddle2Y < 0) paddle2Y = 0;
+                }
+                if (currentKeyStates[SDL_SCANCODE_DOWN]) {
+                    paddle2Y += paddleSpeed;
+                    if (paddle2Y > SCREEN_HEIGHT - PADDLE_HEIGHT) paddle2Y = SCREEN_HEIGHT - PADDLE_HEIGHT;
+                }
+            } else {
+                updateAIPaddle();
+            }
+
+            updateBall();
+
+            if (checkWinCondition(renderer, font)) {
+                gameRunning = false;
+            }
+
+            render(renderer, font);
+            SDL_Delay(10);
+        }
     }
+
 
     close(window, renderer, font);
+
     return 0;
 }
